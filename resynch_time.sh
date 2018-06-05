5e736f79b097d9de06e6730559d250ec07ba6d34
abhishek <chunnuabhishek21@gmail.com>
2018-06-05 22:27:59 +0530

    modified script

--- resynch_time.sh
+++ resynch_time.sh
@@ -1,15 +1,8 @@
 # This script will run on a Jenkins Linux slave and resynchronizes the NTP offset.
 
-cat <<EOF > remote.sh
 # Set the PATH and TERM because piping a script to sshpass via STDIN does not initialize a terminal
-export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
-export TERM=xterm
- 
+
 sudo cat /etc/resolv.conf > resolv.txt
-
 cat resolv.txt
-
-sudo service ntpd restartS
-
-EOF
+sudo service ntpd restart
 echo "the ip is $ip denote it"
